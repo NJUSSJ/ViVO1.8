6ab310b319130151644171456182d64ac4fe8ab6
Yang <yyqyang222@163.com>
2019-05-07 20:52:35 +0800

    Custom: getStatement()

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -38,6 +38,72 @@ public class Customer {
     //获取所有账户详情
     public String getStatement() {
         String statement = "";
+        double checkingDeposit = 0.00, checkingWithdrawal = 0.00,
+            savingDeposit = 0.00, savingWithdrawal = 0.00,
+            maxiDeposit = 0.00, maxiWithdrawal= 0.00;
+
+
+        // 遍历所有账户
+        for (Account account: accounts) {
+
+            // 遍历每一个账户的交易详情
+            for (Transaction transaction: account.transactions ) {
+                switch (account.getAccountType()) {
+
+                    case Account.CHECKING:{
+                        if (transaction.amount > 0) {
+                            checkingDeposit += transaction.amount;
+                        }
+                        else {
+                            checkingWithdrawal += Math.abs(transaction.amount); // 取绝对值
+                        }
+                        break;
+                    }
+
+                    case Account.SAVINGS:{
+                        if (transaction.amount > 0) {
+                            savingDeposit += transaction.amount;
+                        }
+                        else {
+                            savingWithdrawal += Math.abs(transaction.amount); // 取绝对值
+                        }
+                        break;
+                    }
+
+                    case Account.MAXI_SAVINGS:{
+                        if (transaction.amount > 0) {
+                            maxiDeposit += transaction.amount;
+                        }
+                        else {
+                            maxiWithdrawal += Math.abs(transaction.amount); // 取绝对值
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        statement += "Statement for "+ this.name +"\n" +
+                "\n" +
+                "Checking Account\n" +
+                (checkingDeposit != 0.00? "  deposit $" + checkingDeposit + "\n": "") +
+                (checkingWithdrawal != 0.00? "  withdrawal" + checkingWithdrawal + "\n": "") +
+                "Total $" + (checkingDeposit-checkingWithdrawal) + "\n" +
+                "\n" +
+                "Savings Account\n" +
+                (savingDeposit != 0.00? "  deposit $" + savingDeposit + "\n": "") +
+                (savingWithdrawal != 0.00?"  withdrawal $" + savingWithdrawal + "\n": "") +
+                "Total $" + (savingDeposit - savingWithdrawal) + "\n" +
+                "\n" +
+                "Maxi Savings Account\n" +
+                (maxiDeposit != 0.00? "  deposit $" + maxiDeposit + "\n": "") +
+                (maxiWithdrawal != 0.00? "  withdrawal $" + maxiDeposit + "\n": "") +
+                "Total $" + (maxiDeposit - maxiWithdrawal) + "\n" +
+                "\n" +
+                "Total In All Accounts $" + (checkingDeposit-checkingWithdrawal) +
+                (savingDeposit - savingWithdrawal) + (maxiDeposit - maxiWithdrawal);
+
         return statement;
     }
         
