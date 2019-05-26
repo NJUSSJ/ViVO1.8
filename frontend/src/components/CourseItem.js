556e31741bbb0b978ce98cc9a822768025870422
Fortune <njussj@gmail.com>
2019-05-26 20:07:21 +0800

    CourseItem

--- frontend/src/components/CourseItem.js
+++ frontend/src/components/CourseItem.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, Button, Alert, Image} from 'react-native';
 import API from '../utils/methods'
+import StarRating from './StarRating'
 
 export default class CourseItem extends React.Component{
     constructor(props) {
@@ -10,10 +11,14 @@ export default class CourseItem extends React.Component{
     render() {
         return (
             <View style={styles.container}>
-                <Image source={this.props.picUrl}/>
-                <View>
-                    <Text>{this.props.courseName}</Text>
-                    <Text>{this.props.department}{this.props.teacher}</Text>
+                <Image source={this.props.picUrl} style={{height: 70, width: 70, position: 'absolute', left: 15}}/>
+                <View style={{position: 'absolute', left: 90}}>
+                    <Text style={{fontSize: 25}}>{this.props.courseName}</Text>
+                    <Text style={{marginTop: 5}}>{this.props.department}、{this.props.teacher}</Text>
+                </View>
+                <View style={{marginLeft: 10, flexDirection: 'row', position: 'absolute', right: 15}}>
+                    <StarRating maxStars={5} rating={this.props.overallScore} disabled={true} starSize={25} style={{marginTop: 20}}/>
+                    <Text style={{marginTop: 5, marginLeft:10, fontSize: 20}}>{this.props.overallScore}</Text>
                 </View>
             </View>
         );
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
 import API from '../utils/methods'
+import CourseItem from '../components/CourseItem'
 export default class Login extends Component {
     constructor() {
         super();
@@ -16,11 +17,17 @@ export default class Login extends Component {
             <View style={styles.container}>
                 <View style={styles.loginPane}>
                     <Text style={{fontSize: 20}}>用户名:</Text>
-                    <TextInput style={styles.inputArea} onChangeText={(text) => {this.setState({username: text})}}/>
+                    <TextInput style={styles.inputArea} onChangeText={(text) => {
+                        this.setState({username: text})
+                    }}/>
                     <Text style={{fontSize: 20}}>密码:</Text>
-                    <TextInput style={styles.inputArea} secureTextEntry={true} onChangeText={(text) => {this.setState({password: text})}}/>
+                    <TextInput style={styles.inputArea} secureTextEntry={true} onChangeText={(text) => {
+                        this.setState({password: text})
+                    }}/>
                     <Button title="登录" onPress={this.login} color='#be8dbd'/>
-                    <Text style={{marginTop: 20, marginLeft: 80}} onPress={()=>{this.props.navigation.navigate('SignUp')}}>没有账号？去注册一个</Text>
+                    <Text style={{marginTop: 20, marginLeft: 80}} onPress={() => {
+                        this.props.navigation.navigate('SignUp')
+                    }}>没有账号？去注册一个</Text>
                 </View>
             </View>
         );
